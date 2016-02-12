781a3a2f5dc9e4a4147430365aef1548805264b0
MuXian Wu <muxian.wu@gogame.net>
2016-02-12 12:31:22 +0800

    Added sh

---
+++ setup.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/ bash
+
+setup_homebrew() {
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+}
+
+setup_homebrew_packages() {
+    brew install python
+    brew install wget
+    brew install go
+    brew install cmake
+    brew install youtube-dl
+    brew install jenv 
+    brew install ansible
+}
+
+setup_python_packages() {
+    sudo easy_install pip
+    sudo pip install -U pip
+    sudo pip install boto
+}
