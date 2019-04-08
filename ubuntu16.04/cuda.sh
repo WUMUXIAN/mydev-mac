6beccdf06beffcd8865c105bc315e3fe8cc6eb61
WUMUXIAN <wumuxian1988@gmail.com>
2019-04-08 12:26:12 +0800

    modified cuda installation shell script

--- ubuntu16.04/cuda.sh
+++ ubuntu16.04/cuda.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+## install kernel headers and download cuda installers.
 sudo apt-get install linux-headers-$(uname -r) -y
 wget https://developer.nvidia.com/compute/cuda/10.1/Prod/local_installers/cuda_10.1.105_418.39_linux.run
 wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/local_installers/cuda_8.0.61_375.26_linux-run
@@ -8,12 +10,12 @@ wget https://developer.nvidia.com/compute/cuda/8.0/Prod2/patches/2/cuda_8.0.61.2
 sudo sh -c 'echo "blacklist nouveau\noptions nouveau modeset=0" > /etc/modprobe.d/blacklist-nouveau.conf'
 sudo update-initramfs -u
 
-#### please follow the official instructions to install.
+#### Please note that after this step we need to restart the machine.
+#### If might not be able to boot, we need to move into terminal mode by ctrl+alt+F2.
+#### In terminal mode, run the following:
 
-
-### The following command is used.
-sudo sh cuda_10.1.105_418.39_linux.run --silent --driver --toolkit --toolkitpath=/usr/local/cuda-10.0
-sudo sh cuda_8.0.61_375.26_linux.run --silent --toolkit --toolkitpath=/usr/local/cuda-8.0
-sudo sh cuda_8.0.61.2_linux.run
+sudo sh cuda_10.1.105_418.39_linux-run --silent --driver --toolkit --toolkitpath=/usr/local/cuda-10.0
+sudo sh cuda_8.0.61_375.26_linux-run --silent --toolkit --toolkitpath=/usr/local/cuda-8.0
+sudo sh cuda_8.0.61.2_linux-run
 
 export PATH=$PATH:/usr/local/cuda/bin
